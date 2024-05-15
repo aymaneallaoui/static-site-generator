844610b4ef338b55701b453dd58e91960bf78df6
aymaneallaoui <aymanepraxe@gmail.com>
2024-05-15 18:19:11 +0100

    first commit

---
+++ src/main.py
@@ -0,0 +1,3 @@
+from textnode import TextNode
+
+TextNode("This is a text node", "bold" , "https://www.boot.dev")
---
+++ src/textnode.py
@@ -0,0 +1,13 @@
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if isinstance(other, TextNode):
+            return (self.text == other.text) and (self.text_type == other.text_type) and (self.url == other.url)
+        return False
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
