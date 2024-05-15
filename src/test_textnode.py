66e4ecdff436ef9f1ebc6f8133c5badb0c589b04
aymaneallaoui <aymanepraxe@gmail.com>
2024-05-15 19:25:01 +0100

    so far so good

---
+++ src/test_textnode.py
@@ -0,0 +1,60 @@
+import unittest
+
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
+)
+
+from textnode import split_nodes_delimiter
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node", text_type_text)
+        self.assertEqual(node, node2)
+
+    def test_eq_false(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node", text_type_bold)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node2", text_type_text)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is text with a *code block* word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "*", text_type_bold)
+        expected_nodes = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("code block", text_type_bold),
+            TextNode(" word", text_type_text),
+        ]
+        self.assertEqual(new_nodes, expected_nodes)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,14 @@
+from htmlnode import LeafNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -11,3 +22,32 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == text_type_image:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+        else:
+            split_text = node.text.split(delimiter)
+            for i, text in enumerate(split_text):
+                new_type = text_type if i % 2 else text_type_text
+                new_nodes.append(TextNode(text, new_type))
+    return new_nodes
