20792ffbd8a153e6bd99831ff59a9f045115a364
aymaneallaoui <aymanepraxe@gmail.com>
2024-05-17 19:19:53 +0100

    text_to_textnodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -10,7 +10,8 @@ from textnode import (
     text_type_link,
 )
 
-from textnode import split_nodes_delimiter , extract_markdown_links, extract_markdown_images , split_nodes_image , split_nodes_link
+from textnode import split_nodes_delimiter, extract_markdown_links, extract_markdown_images, split_nodes_image, split_nodes_link, text_to_textnodes
+
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -29,24 +30,24 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
     def test_eq_url(self):
-        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
-        node2 = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        node = TextNode("This is a text node", text_type_text,
+                        "https://www.boot.dev")
+        node2 = TextNode("This is a text node",
+                         text_type_text, "https://www.boot.dev")
         self.assertEqual(node, node2)
 
     def test_repr(self):
-        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        node = TextNode("This is a text node", text_type_text,
+                        "https://www.boot.dev")
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
 
 
-
-
-
-
 class TestSplitNodesDelimiter(unittest.TestCase):
     def test_split_nodes_delimiter(self):
-        node = TextNode("This is text with a *code block* word", text_type_text)
+        node = TextNode(
+            "This is text with a *code block* word", text_type_text)
         new_nodes = split_nodes_delimiter([node], "*", text_type_bold)
         expected_nodes = [
             TextNode("This is text with a ", text_type_text),
@@ -72,7 +73,8 @@ class TestExtractMarkdownLinks(unittest.TestCase):
     def test_extract_markdown_images_multiple_images(self):
         text = "This is a text with an image ![image](https://www.boot.dev) and another ![image2](https://www.boot.dev/2)"
         images = extract_markdown_images(text)
-        expected_images = [("image", "https://www.boot.dev"), ("image2", "https://www.boot.dev/2")]
+        expected_images = [("image", "https://www.boot.dev"),
+                           ("image2", "https://www.boot.dev/2")]
         self.assertEqual(images, expected_images)
 
     def test_extract_markdown_links(self):
@@ -90,14 +92,15 @@ class TestExtractMarkdownLinks(unittest.TestCase):
     def test_extract_markdown_links_multiple_links(self):
         text = "This is a text with a link [link](https://www.boot.dev) and another [link2](https://www.boot.dev/2)"
         links = extract_markdown_links(text)
-        expected_links = [("link", "https://www.boot.dev"), ("link2", "https://www.boot.dev/2")]
+        expected_links = [("link", "https://www.boot.dev"),
+                          ("link2", "https://www.boot.dev/2")]
         self.assertEqual(links, expected_links)
-
 
 
 class TestSplitNodesImage(unittest.TestCase):
     def test_split_nodes_image(self):
-        node = TextNode("This is text with an image ![image](https://www.boot.dev)", text_type_text)
+        node = TextNode(
+            "This is text with an image ![image](https://www.boot.dev)", text_type_text)
         new_nodes = split_nodes_image([node])
         expected_nodes = [
             TextNode("This is text with an image ", text_type_text),
@@ -112,7 +115,8 @@ class TestSplitNodesImage(unittest.TestCase):
         self.assertEqual(new_nodes, expected_nodes)
 
     def test_split_nodes_image_multiple_images(self):
-        node = TextNode("This is text with an image ![image](https://www.boot.dev) and another ![image2](https://www.boot.dev/2)", text_type_text)
+        node = TextNode(
+            "This is text with an image ![image](https://www.boot.dev) and another ![image2](https://www.boot.dev/2)", text_type_text)
         new_nodes = split_nodes_image([node])
         expected_nodes = [
             TextNode("This is text with an image ", text_type_text),
@@ -123,10 +127,10 @@ class TestSplitNodesImage(unittest.TestCase):
         self.assertEqual(new_nodes, expected_nodes)
 
 
-
 class TestSplitNodesLink(unittest.TestCase):
     def test_split_nodes_link(self):
-        node = TextNode("This is text with a link [link](https://www.boot.dev)", text_type_text)
+        node = TextNode(
+            "This is text with a link [link](https://www.boot.dev)", text_type_text)
         new_nodes = split_nodes_link([node])
         expected_nodes = [
             TextNode("This is text with a link ", text_type_text),
@@ -141,7 +145,8 @@ class TestSplitNodesLink(unittest.TestCase):
         self.assertEqual(new_nodes, expected_nodes)
 
     def test_split_nodes_link_multiple_links(self):
-        node = TextNode("This is text with a link [link](https://www.boot.dev) and another [link2](https://www.boot.dev/2)", text_type_text)
+        node = TextNode(
+            "This is text with a link [link](https://www.boot.dev) and another [link2](https://www.boot.dev/2)", text_type_text)
         new_nodes = split_nodes_link([node])
         expected_nodes = [
             TextNode("This is text with a link ", text_type_text),
@@ -150,6 +155,27 @@ class TestSplitNodesLink(unittest.TestCase):
             TextNode("link2", text_type_link, "https://www.boot.dev/2"),
         ]
         self.assertEqual(new_nodes, expected_nodes)
+
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_text_to_textnodes(self):
+        text = "This is **text ** with an * italic * word and a `code block` and an ![image](https: // storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a[link](https: // boot.dev)"
+        nodes = text_to_textnodes(text)
+        expected_nodes = [
+            TextNode("This is ", text_type_text),
+            TextNode("text ", text_type_bold),
+            TextNode(" with an ", text_type_text),
+            TextNode(" italic ", text_type_italic),
+            TextNode(" word and a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" and an ", text_type_text),
+            TextNode("image", text_type_image,
+                     "https: // storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and a", text_type_text),
+            TextNode("link", text_type_link, "https: // boot.dev"),
+        ]
+        self.assertEqual(nodes, expected_nodes)
+
+
 if __name__ == "__main__":
     unittest.main()
-
--- src/textnode.py
+++ src/textnode.py
@@ -128,3 +128,13 @@ def split_nodes_link(old_nodes):
                 if text_after:
                     new_nodes.append(TextNode(text_after, text_type_text))
     return new_nodes
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
